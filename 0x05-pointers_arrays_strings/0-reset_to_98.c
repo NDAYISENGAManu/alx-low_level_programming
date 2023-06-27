31c0aa0bd7e9bd54a6ebc0c9850c03a51cc269a6
NDAYISENGAManu <webmasterio2015@gmail.com>
2023-06-26 19:23:28 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 -  takes a parameter to an int and updates
+ * the values it point to 98 
+ *
+ * @n: an integer input point
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
