0cfaebe4c77ac5e9d3efd3df88b79528dcf81953
NDAYISENGAManu <webmasterio2015@gmail.com>
2023-08-01 08:37:54 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,6 +24,12 @@ typedef struct listint_s
  * Description: singly linked list of pointers
  */
 
+typedef struct listp_s
+{
+    void *p;
+    struct listp_s *next;
+} listp_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
