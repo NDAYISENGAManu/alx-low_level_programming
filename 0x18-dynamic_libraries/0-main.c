cbfbb95317aa38e8d1133381d756daebb9352dde
NDAYISENGAManu <webmasterio2015@gmail.com>
2023-09-18 16:18:43 +0200

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @EXIT_SUCCESS : this is a variable to return
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return EXIT_SUCCESS;
+}
